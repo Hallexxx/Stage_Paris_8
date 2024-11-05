0cebd2e16d7c4abde7077c60274420d19bb642a5
Alexandre Perez <112977841+Hallexxx@users.noreply.github.com>
2024-11-05 18:07:15 +0100

    push

--- web_py/static/js/archive.js
+++ web_py/static/js/archive.js
@@ -1,4 +1,8 @@
 function showSection(section) {
     document.getElementById('archiveNews').style.display = section === 'news' ? 'block' : 'none';
     document.getElementById('archiveUsers').style.display = section === 'users' ? 'block' : 'none';
+    document.getElementById('newsButton').classList.toggle('active', section === 'news');
+    document.getElementById('usersButton').classList.toggle('active', section === 'users');
 }
+
+showSection('news');
