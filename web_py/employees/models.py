854b56cdcdebde5127e1fd0107b2ff6790cb54f8
Alexandre Perez <112977841+Hallexxx@users.noreply.github.com>
2025-02-28 14:55:04 +0100

    push

--- web_py/employees/models.py
+++ web_py/employees/models.py
@@ -1,4 +1,4 @@
-from django import models
+from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
 from django.contrib.auth.hashers import make_password
